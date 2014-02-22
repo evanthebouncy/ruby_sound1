dd6a106a4d43a3b08a26079bbf814dfde3151c6c
evan <evan@evan-box.(none)>
2014-02-21 21:35:17 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sound1::Application.config.secret_key_base = '98855f9b08cef22bafa7819ff34e4586e242b63096642c7202e5944b2fedef8717c25f9aad1ebb3059e9036b98762c7d6aca12d4bebf08a1f7961f374c289cd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sound1::Application.config.session_store :cookie_store, key: '_sound1_session'
