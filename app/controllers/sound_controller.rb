c71c4fcda40821ec62f87a2bf4221878a0d617f7
evan <evan@evan-box.(none)>
2014-02-22 21:55:43 -0800

    descriptive msg2

--- app/controllers/sound_controller.rb
+++ app/controllers/sound_controller.rb
@@ -2,7 +2,10 @@ class SoundController < ApplicationController
   def playsound
     @resource_id = params[:resource_id]
     @sender_name = params[:sender_name]
-    @base_resource_path = 'https://digit4me.drinkchat.test1-voice.s3.amazonaws.com/'
+    @base_resource_path = 'https://s3-us-west-2.amazonaws.com/digit4me.drinkchat.test1-voice/'
+    @sound_format = '.mp3'
+
+    @resource_location = "#{@base_resource_path}#{@resource_id}#{@sound_format}"
   #params[:sender_name]
   end
 end
