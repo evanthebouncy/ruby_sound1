1c43bc714848dcbb241745931616f595a8e4739e
evan <evan@evan-box.(none)>
2014-02-22 21:24:57 -0800

    descriptive msg

--- app/controllers/sound_controller.rb
+++ app/controllers/sound_controller.rb
@@ -2,6 +2,7 @@ class SoundController < ApplicationController
   def playsound
     @resource_id = params[:resource_id]
     @sender_name = params[:sender_name]
+    @base_resource_path = 'https://digit4me.drinkchat.test1-voice.s3.amazonaws.com/'
   #params[:sender_name]
   end
 end
